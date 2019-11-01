1398c06781ceecfb9fc6d3ce525d816329ea988c
Your Name <you@example.com>
2019-11-01 14:29:46 +0700

    nopbaitap

---
+++ src/Testpoint.java
@@ -0,0 +1,12 @@
+public class Testpoint {
+    public static void main(String[] args) {
+        Point point = new Point();
+        point.setXY(4.0f,5.0f);
+        System.out.println(point);
+
+
+
+//    public static void main(String[] args) {
+
+    }
+}
---
+++ src/testMovablePoint.java
@@ -0,0 +1,8 @@
+public class testMovablePoint {
+    public static void main(String[] args) {
+        MovablePoint movablePoint = new MovablePoint();
+        movablePoint.setXY(4.3f,4.5f);
+        movablePoint.setSpeed(10.0f,12.0f);
+        System.out.println(movablePoint);
+    }
+}
